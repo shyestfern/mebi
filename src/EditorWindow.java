38ba1083c2288f9c1e3431853dee0a2c5898eec4
shyestfern <zalesak_maxmilian@oauh.cz>
2026-05-07 14:56:30 +0200

    vykreslení okna

---
+++ src/EditorWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class EditorWindow {
+    private JPanel rootPanel;
+    private JScrollPane scrollPane;
+    private JTextArea textArea;
+    private JPanel editorPanel;
+
+    public JPanel getRootPanel(){
+        return rootPanel;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mebi");
+        EditorWindow editorWindow = new EditorWindow();
+        frame.setContentPane(editorWindow.getRootPanel());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
