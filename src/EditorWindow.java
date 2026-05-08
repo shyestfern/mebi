1966014196e2b8c6f507ed8febb867e3c5984040
shyestfern <zalesak_maxmilian@oauh.cz>
2026-05-09 00:24:43 +0200

    přidán JMenuBar s možností ukončení

--- src/EditorWindow.java
+++ src/EditorWindow.java
@@ -9,4 +9,28 @@ public class EditorWindow {
     public JPanel getRootPanel(){
         return rootPanel;
     }
+
+    public JMenuBar vytvorMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu souborMenu = new JMenu("Soubor");
+
+        JMenuItem ukoncitItem = new JMenuItem("Ukončit");
+
+        ukoncitItem.addActionListener(e -> ukoncit());
+
+        ukoncitItem.setAccelerator(
+                KeyStroke.getKeyStroke("ctrl Q")
+        );
+
+        souborMenu.add(ukoncitItem);
+
+        menuBar.add(souborMenu);
+
+        return menuBar;
+    }
+
+    private void ukoncit(){
+        System.exit(0);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("mebi");
         EditorWindow editorWindow = new EditorWindow();
+        frame.setJMenuBar(editorWindow.vytvorMenuBar());
         frame.setContentPane(editorWindow.getRootPanel());
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
