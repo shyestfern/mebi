58dd27f36e60c5ea8c20b3981c0b2a3349478b9a
shyestfern <zalesak_maxmilian@oauh.cz>
2026-05-09 16:05:36 +0200

    přidání informačního dialogu

--- src/EditorWindow.java
+++ src/EditorWindow.java
@@ -14,23 +14,41 @@ public class EditorWindow {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu souborMenu = new JMenu("Soubor");
+        JMenu napovedaMenu = new JMenu("Nápověda");
 
         JMenuItem ukoncitItem = new JMenuItem("Ukončit");
+        JMenuItem infoItem = new JMenuItem("O programu");
 
         ukoncitItem.addActionListener(e -> ukoncit());
+        infoItem.addActionListener(e -> zobrazInfo());
 
         ukoncitItem.setAccelerator(
                 KeyStroke.getKeyStroke("ctrl Q")
         );
 
         souborMenu.add(ukoncitItem);
+        napovedaMenu.add(infoItem);
 
         menuBar.add(souborMenu);
+        menuBar.add(napovedaMenu);
 
         return menuBar;
     }
 
     private void ukoncit(){
         System.exit(0);
+    }
+
+    private void zobrazInfo(){
+        JOptionPane.showMessageDialog(rootPanel,
+                "mebi\n" +
+                        "Verze 1.0\n" +
+                        "Textový editor s GUI\n\n" +
+                        "Autor: shyestfern\n" +
+                        "Rok: 2026\n" +
+                        "GitHub: https://github.com/shyestfern/mebi",
+                        "O programu",
+                        JOptionPane.INFORMATION_MESSAGE
+        );
     }
 }
